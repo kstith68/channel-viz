868402a69dfca0c03df01b4710d2ac36332d2b2c
kstith68 <kstith@yahoo.com>
2014-03-26 21:03:53 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,8 +15,8 @@
 	var defaultKey		= 'HrLlg9A3xjgdwCFRAZjBpubBPuHpzxlI94obUZPEZmT6zoQ9', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1323997008], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'RISC Data Service', // Replaces Xively logo in the header
-		dataDuration	= '1hour', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '2hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
